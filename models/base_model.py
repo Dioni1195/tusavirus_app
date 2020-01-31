6dcbdd1faec22a6e2975bdaf144e1e67f98b5088
Dioni1195 <dionisio.arango95@gmail.com>
2020-01-31 20:52:47 +0000

    Adding essentials

---
+++ models/base_model.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""This is the base model class for AirBnB"""
+import uuid
+import models
+from datetime import datetime
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class BaseModel:
+    """This class will defines all common attributes/methods
+    for other classes
+    """
+    id = Column(String(60), unique=True, nullable=False, primary_key=True)
+    created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+
+    def __init__(self):
+        """Instantiation of base model class
+        Args:
+            args: it won't be used
+            kwargs: arguments for the constructor of the BaseModel
+        Attributes:
+            id: unique id generated
+            created_at: creation date
+            updated_at: updated date
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = self.updated_at = datetime.now()
+
+    def __str__(self):
+        """returns a string
+        Return:
+            returns a string of class name, id, and dictionary
+        """
+        return "[{}] ({}) {}".format(
+            type(self).__name__, self.id, self.to_dict())
+
+    def __repr__(self):
+        """return a string representaion
+        """
+        return self.__str__()
+
+    def save(self):
+        """updates the public instance attribute updated_at to current
+        """
+        self.updated_at = datetime.now()
+        models.storage.new(self)
+        models.storage.save()
